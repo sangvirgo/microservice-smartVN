55b990ba347a2f734590ea8bc4b1cd887d86197c
Sang <tansang06092004@gmail.com>
2025-09-18 23:21:58 +0700

    (init): init project

--- eureka-server/src/main/java/com/smartvn/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/smartvn/eureka_server/EurekaServerApplication.java
@@ -1,14 +1,14 @@
-package com.smartvn.api_gateway;
+package com.smartvn.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;	
 @SpringBootApplication
 @EnableEurekaServer
-public class ApiGatewayApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
