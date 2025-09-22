e0ea3d648c8a3206a514933ba1b4014d77d0fcd7
Sang <tansang06092004@gmail.com>
2025-09-23 06:52:04 +0700

    22-09-2025

--- eureka-server/src/main/java/com/smartvn/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/smartvn/eureka_server/EurekaServerApplication.java
@@ -2,7 +2,8 @@ package com.smartvn.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;	
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+	
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
